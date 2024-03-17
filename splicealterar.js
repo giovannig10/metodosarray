5bbbfa2e95450af532ce1ff45d63f79082cef58b
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:53:10 -0300

    exemplo splice alterar

---
+++ splicealterar.js
@@ -0,0 +1,5 @@
+//com este exemplo eu estou removendo a fruta guarana para adcionar a fruta morango
+let frutas = ["Banana", "Uva", "Maça", "Laranja", "Guarana"];
+
+frutas.splice(4,1, "Morango",);
+console.log(frutas);
