2ee4c5c4815c13880b5fa19813b6ebc506128d8a
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:45:18 -0300

    exemplo um reverse

---
+++ reverse.js
@@ -0,0 +1,7 @@
+//serve para reverter o array
+//meu array vai ser com numeros entao se a sequencia é 1,2,3,4
+//ele vai reverter,ou seja, vai se transformar em 4,3,2,1
+const numeros = [1, 2, 3, 4];
+const invertido = numeros.reverse();
+
+console.log(numeros);
