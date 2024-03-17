05cb1173ac7e6e3d0eb967e7b9bb132dbc388701
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:53:31 -0300

    exemplo splice remover

---
+++ spliceremover.js
@@ -0,0 +1,6 @@
+//no exemplo abaixo usamos a variavel o metodo splice para remover um sobrenome no meio de uma lista com outros nomes
+
+let nomes = ["giovanni", "jose", "maris", "goncalves"];
+let sobrenome = nomes.splice(3);
+console.log(sobrenome);
+console.log(nomes);
