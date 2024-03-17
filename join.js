25c6bff498553835c660c03b6bf5d96c61511548
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:43:07 -0300

    exemplo um join

---
+++ join.js
@@ -0,0 +1,4 @@
+//join serve para juntar elementos e adicionar algo entre eles
+//neste primeiro exemplo iremos adicionar im (-) entre uma lista de times de futebol
+let times = ['pameiras', 'guarani', 'corinthians', 'flamengo']
+console.log(times.join(' - '));
