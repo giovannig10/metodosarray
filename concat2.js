347c48679416ca22de62ec2946347348e2986cf5
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:38:21 -0300

    exemplo dois concat

---
+++ concat2.js
@@ -0,0 +1,4 @@
+//nesse exemplo de concat vamos concatenar uma string a outra string
+//com isso vamos ter uma terceira string
+let fala = "fala,";
+console.log(fala.concat("zeze ", "bom dia cara."));
