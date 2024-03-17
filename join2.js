cd78c0f9c722c0b7655c481da9f9e448648f3a75
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:43:29 -0300

    exemplo dois join

---
+++ join2.js
@@ -0,0 +1,13 @@
+//neste exemplo adicionei a letra e entre todas cores que eu acho legal
+//mas poderia ter adicionado outras coisas, uma virgula por exemplo
+const cores = [
+    "preto",
+    "azul",
+    "vermelho",
+    "rosa",
+    "amarelo",
+    "roxo",
+];
+let coresBonitas = cores.join(" e ");
+console.log(cores);
+console.log(coresBonitas);
