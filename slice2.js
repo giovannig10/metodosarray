8137a019795318a52d1b108360db933a3019d02e
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:49:40 -0300

    exemplo dois slice

---
+++ slice2.js
@@ -0,0 +1,6 @@
+//neste exemplo ao inves de fazermos com nomes
+//iremos fazer marcas 
+//iremso copiar de uma lista as marcas que eu mais uso
+let marcas = ["nike", "adidas", "puma", "topper", "rainha", "diadora"];
+let marcasFavoritas = marcas.slice(0, 3);
+console.log(marcasFavoritas);
