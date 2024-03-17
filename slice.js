c12756ca38686341cd71f43af3de3f0c3929881f
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:49:09 -0300

    exemplo um slice

---
+++ slice.js
@@ -0,0 +1,4 @@
+//o metodo slice serve para fazer uma copia de uma parte de seu array
+let nomes = ["giovanni", "jose", "ana" , "julia", "vinicius"];
+let familia = nomes.slice(0, 2);
+console.log(familia);
