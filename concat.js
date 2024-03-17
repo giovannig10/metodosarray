a2f5b5b6b65f6760a35e84caec7ded871d2b6b81
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:34:55 -0300

    exemplo um concat

---
+++ concat.js
@@ -0,0 +1,9 @@
+//10 metodos array
+//.concat ()
+// o metodo concat serve para combinar varios arrays em um so array
+// no metodo abaixo vamos concatenar valores em um array
+const nomes = ['giovanni', 'ana', 'julia'];
+
+const numeros = nomes.concat(1, [2, 3]);
+
+console.log(numeros);
