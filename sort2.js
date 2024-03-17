e8dedf83c09d35debe9ddbc07490d8ec4dd32e8b
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:52:04 -0300

    exemplo dois sort

---
+++ sort2.js
@@ -0,0 +1,7 @@
+//neste exemplo de sort vamos utiliza-lo para organizarmos uma lista de numeros
+//de forma crescente
+//tambem pode ser usada de forma decrescente, adicionando um if
+const numeros = [4, 3, 2, 1];
+numeros.sort();
+console.log(numeros);
+
