908ef162b9e0ee392bacb7ceb20fd107bd0aaea4
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-17 17:52:40 -0300

    exemplo splice adicionar

---
+++ spliceadicionar.js
@@ -0,0 +1,5 @@
+//neste exemplo eu estou adicionando morango na lista de frutas
+let frutas = ["Banana", "Uva", "Maça", "Laranja", "Guarana"];
+
+frutas.splice(5,0, "Morango",);
+console.log(frutas);
